9ec5370ec19911feadbb47253f9afc5c775915c2
Spidey03 <sayhai2naveenkumar@gmail.com>
2022-02-18 20:27:33 +0530

    Find unsolved box

--- solver.py
+++ solver.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Solver:
     def __init__(self, problem):
         self.problem = problem
@@ -7,4 +9,15 @@ class Solver:
             raise Exception("Sudoku Problem is not in correct format: expected 9 * 9")
 
     def get_solution(self):
-        self.validate_problem()
+        # self.validate_problem()
+        pos = self.get_unsolved_box()
+        if not pos:
+            print("Find solution!")
+            return True
+        
+            
+    
+    def get_unsolved_box(self):
+        row_indices, col_indices = np.where(self.problem == 10)
+        if row_indices and col_indices:
+            return row_indices[0], col_indices[0]
